abdb16bf6391764402cd9c39ae8a912f83e35807
bluk44 <bluk44@gmail.com>
2012-01-04 18:30:54 +0100

    added netty framework into the action

--- Chat-service/src/server/GameServer.java
+++ Chat-service/src/server/GameServer.java
@@ -10,122 +10,52 @@ import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
 import java.util.Iterator;
 import java.util.Set;
+import java.util.concurrent.Executors;
 
 import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Logger;
+import org.jboss.netty.bootstrap.ServerBootstrap;
+import org.jboss.netty.channel.Channel;
+import org.jboss.netty.channel.ChannelFactory;
+import org.jboss.netty.channel.ChannelPipeline;
+import org.jboss.netty.channel.ChannelPipelineFactory;
+import org.jboss.netty.channel.Channels;
+import org.jboss.netty.channel.group.ChannelGroup;
+import org.jboss.netty.channel.group.ChannelGroupFuture;
+import org.jboss.netty.channel.group.DefaultChannelGroup;
+import org.jboss.netty.channel.socket.nio.NioServerSocketChannelFactory;
 
-public class GameServer implements ServerInterface, Runnable {
 
-	private static int DEF_PORT = 10000;
-	private static int DEF_BSIZE = 1024;
 
-	private ServerSocketChannel serverChannel;
-	private ServerSocket serverSocket;
-	private SocketChannel client;
-	private ByteBuffer readBuffer;
-	private InetSocketAddress serverAddress;
-	private Selector selector;
-	private Logger logger = Logger.getLogger(GameServer.class);
-	{
-		BasicConfigurator.configure();
-	}
+public class GameServer {
+	private static int DEFAULT_PORT = 10000;
+	private ChannelFactory channelFactory;
+	private ServerBootstrap serverBootstrap;
+	private ChannelGroup allChannels = new DefaultChannelGroup("all clients");
 
-	public GameServer() {
-		serverAddress = new InetSocketAddress(DEF_PORT);
-		logger.info("server port set to " + DEF_PORT);
-	}
-
-	public GameServer(int port) {
-		serverAddress = new InetSocketAddress(port);
-		logger.info("server port set to " + port);
-	}
-
-	@Override
-	public void start() throws IOException {
-		logger.info("starting server");
-
-		serverChannel = ServerSocketChannel.open();
-		logger.info("server channel opened");
-
-		serverSocket = serverChannel.socket();
-		serverSocket.bind(serverAddress);
-		logger.info("server socket bound");
-
-		serverChannel.configureBlocking(false);
-		selector = Selector.open();
-		serverChannel.register(selector, SelectionKey.OP_ACCEPT);
-
-		while (true) {
-			selector.select();
-
-			Set readyKeys = selector.selectedKeys();
-			Iterator iterator = readyKeys.iterator();
-
-			while (iterator.hasNext()) {
-				SelectionKey key = (SelectionKey) iterator.next();
-				iterator.remove();
-				// checking type of key
-				if (key.isAcceptable()) {
-					// new client
-					ServerSocketChannel server = (ServerSocketChannel) key
-							.channel();
-					client = server.accept();
-					logger.info("accepted client");
-
-					client.configureBlocking(false);
-					SelectionKey skey = client.register(selector,
-							SelectionKey.OP_READ);
-					readBuffer = ByteBuffer.allocate(DEF_BSIZE);
-					skey.attach(readBuffer);
-
-				} else if (key.isReadable()) {
-					logger.info("a client is ready to read! chuuuuuuuj");
-
-					SocketChannel client = (SocketChannel) key.channel();
-					client.read(readBuffer);
-					readBuffer.flip();
-					String message = new String(readBuffer.array());
-					logger.info("message from client {" + message + "}");
-				}
+	public void start() {
+		channelFactory = new NioServerSocketChannelFactory(
+				Executors.newCachedThreadPool(),
+				Executors.newCachedThreadPool());
+		serverBootstrap = new ServerBootstrap(channelFactory);
+		serverBootstrap.setPipelineFactory(new ChannelPipelineFactory() {
+			public ChannelPipeline getPipeline() {
+				return Channels.pipeline(new ChatServerHandler(allChannels));
 			}
-		}
+		});
 
+		serverBootstrap.setOption("child.tcpNoDelay", true);
+		serverBootstrap.setOption("child.keepAlive", true);
+
+		Channel serverChannel = serverBootstrap.bind(new InetSocketAddress(
+				DEFAULT_PORT));
+		//allChannels.add(serverChannel);
 	}
 
-	@Override
-	public void stop() throws IOException {
-		if (client != null) {
-			client.close();
-			logger.info("client channel closed");
-		}
-		if (serverChannel != null) {
-			logger.info("closing server channel");
-			serverChannel.close();
-			logger.info("server channel closed");
-		}
-
+	public void stop() {
+		ChannelGroupFuture closeFuture = allChannels.close();
+		closeFuture.awaitUninterruptibly();
+		channelFactory.releaseExternalResources();
 	}
-
-	@Override
-	public String getIPAdress() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Integer getPortNumber() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void run() {
-		try {
-			start();
-		} catch (IOException e) {
-			logger.error("exception in start", e);
-		}
-
-	}
-
+	
 }
